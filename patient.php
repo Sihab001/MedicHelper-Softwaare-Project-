2e55952321abaaedf967d85231508de0b3e393ae
Shahadat Ali <mali163091@bscse.uiu.ac.bd>
2021-12-08 04:48:55 +0600

    code added to patient.php

--- patient.php
+++ patient.php
@@ -4,4 +4,15 @@
     }
     include "pdo.php";
 
+    if(isset($_POST["submit"])){
+        if(isset($_POST['password'])){
+                    $name = $_POST["name"];
+        $password = $_POST["password"];
+        $username = $_POST["username"];
+        $contact = $_POST["contact"];
+        $blood_group = $_POST["blood_group"];
+        $weight = $_POST["weight"];
+
+        }
+    }
 ?>
