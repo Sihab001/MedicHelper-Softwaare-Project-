98819b80b638e7cdec907092938ac5dc31c37b85
Shahadat Ali <95707995+MdShahadatAli@users.noreply.github.com>
2021-12-08 03:19:25 +0600

    db code updated

--- db.php
+++ db.php
@@ -1,13 +1,4 @@
 <?php
-$servername = "localhost";
-$username1 = "root";
-$password1 = "";
-$dbname = "medichelper";
-// Create connection
-$conn2 = new mysqli($servername, $username1, $password1, $dbname);
-// Check connection
-if ($conn2->connect_error) {
-    die("Connection failed: " . $conn2->connect_error);
-}
+
 
 ?>
