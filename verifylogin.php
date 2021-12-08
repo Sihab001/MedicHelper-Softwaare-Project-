139d965de4f84ffd41a0d845fd4aec178d54dd91
Sihab <mshakran162084@bscse.uiu.aac.bd>
2021-12-08 12:08:26 +0600

    New function add to verifylogin

---
+++ verifylogin.php
@@ -0,0 +1,46 @@
+<?php
+///database connection 
+    include 'pdo.php';
+
+    if(isset($_POST["submit"])){
+        $password = $_POST["pass"];
+        $username = $_POST["uname"];
+
+        $userquery= "select * from patient where username='$username' AND password='$password' ";
+        $returnvalue=$conn->prepare($userquery);
+        $returnvalue->execute();
+        $rowcount = $returnvalue->rowCount();
+
+        $userquery1= "select * from doctor where username='$username' AND password='$password' ";
+        $returnvalue1=$conn->prepare($userquery1);
+        $returnvalue1->execute();
+        $rowcount1 = $returnvalue1->rowCount();
+        if($rowcount==1){
+            if(!isset($_SESSION)){
+                session_start();
+            }
+            $_SESSION['user']=$username;
+            echo '<script>
+            window.location.href="table1.php";
+            </script>';
+            
+        }
+        else if($rowcount1==1){
+            if(!isset($_SESSION)){
+                session_start();
+            }
+            $_SESSION['user']=$username;
+            echo '<script>
+            window.location.href="table2.php";
+            </script>';
+            
+        }
+        else{
+            echo '<script>
+            window.location.href="login.php";
+            </script>';
+        }
+    }
+	
+
+?>
