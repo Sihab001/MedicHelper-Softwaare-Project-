d57797b4a3b0d4a4d09d1c3052a8ae8b5c66bbb6
Mukta Moly <muktaakanda2014@gmail.com>
2021-12-08 13:00:46 +0600

    New foder created

---
+++ pateint.php
@@ -0,0 +1,50 @@
+
+<html>
+<head>
+    
+<title>insert from data in mysql database using php</title>
+    
+</head>
+<body style="background-color:cadetblue; align-items: center;display: flex;
+justify-content:space-between;
+flex-direction: column;
+align-items: center;
+">
+
+    <form action="" method ="post">
+
+    <table>
+        <tr>
+            <th></th>
+            <th><h2>For New Patient Registration</h2></th>
+        </tr>
+        <tr>
+            <td>Name:</td>
+            <td><input autocomplete="off" type ="text" name ="name"></td>
+        </tr>
+        <tr>
+            <td> User Name:</td>
+            <td><input autocomplete="off" type ="text" name ="username"></td>
+        </tr>
+        <tr>
+            <td>Mobile Number:</td>
+            <td><input autocomplete="off" type ="text" name ="contact"></td>
+        </tr>
+        <tr>
+            <td> Bloodgroup:</td>
+            <td><input autocomplete="off" type ="text" name ="blood_group"></td>
+        </tr>
+        <tr>
+            <td> Weight:</td>
+            <td><input autocomplete="off" type ="number" name ="weight"></td>
+        </tr>
+	
+       
+
+    
+       
+ </table> </form>
+ <button style="width:200px;">Done</button>
+ <button style="width:200px;">Check Status</button>
+ </body>
+</html>
